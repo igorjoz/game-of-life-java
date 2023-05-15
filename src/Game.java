4e8dfa9d6d527427b7d99b83984bf12cefed2c65
Igor Józefowicz <igorjozefowicz@gmail.com>
2023-05-15 23:12:53 +0200

    develop Game class

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Game {
     private GUI gui;
     private int size;
 
+
+    private World world;
+
+    private char playerInput;
+    private int turn;
+    private boolean isRunning;
+
+    private int specialAbilityCooldown;
+    private boolean isSpecialAbilityActive;
+
+    private static final int SPECIAL_ABILITY_COOLDOWN = 5;
+
     public Game() {
     }
 
+    /*public Game(int size) {
+        this.world = new World(size);
+        this.playerInput = ' ';
+        this.isRunning = true;
+        this.turn = 0;
+        this.specialAbilityCooldown = -1;
+        this.isSpecialAbilityActive = false;
+        this.size = size;
+    }*/
+
     public Game(GUI gui) {
         this.gui = gui;
-        //this.size = gui.getMapSize();
     }
 
     public void runGame() {
@@ -15,6 +39,14 @@ public class Game {
     }
 
     public void initializeGame(int size) {
+        this.size = size;
+
+        this.world = new World(size);
+        this.playerInput = ' ';
+        this.isRunning = true;
+        this.turn = 0;
+        this.specialAbilityCooldown = -1;
+        this.isSpecialAbilityActive = false;
         this.size = size;
 
         // create 2D array of size x size
